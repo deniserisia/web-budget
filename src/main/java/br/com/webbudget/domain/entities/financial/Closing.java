6c3a87515c8457338590f1884c3b498da82819c5
Denise Risia <risiaresponde@gmail.com>
2023-06-14 16:41:54 -0300

    Add da anotação @Data p/ gerar automaticamente os metodos etc, rm anotações não relevantes

--- src/main/java/br/com/webbudget/domain/entities/financial/Closing.java
+++ src/main/java/br/com/webbudget/domain/entities/financial/Closing.java
@@ -19,10 +19,7 @@ package br.com.webbudget.domain.entities.financial;
 import br.com.webbudget.domain.entities.PersistentEntity;
 import br.com.webbudget.domain.entities.registration.FinancialPeriod;
 import br.com.webbudget.infrastructure.utils.RandomCode;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 import org.hibernate.envers.AuditTable;
 import org.hibernate.envers.Audited;
 
@@ -33,14 +30,7 @@ import java.time.LocalDate;
 import static br.com.webbudget.infrastructure.utils.DefaultSchemes.FINANCIAL;
 import static br.com.webbudget.infrastructure.utils.DefaultSchemes.FINANCIAL_AUDIT;
 
-/**
- * The result of the accounting for a {@link FinancialPeriod}
- *
- * @author Arthur Gregorio
- *
- * @version 3.0.0
- * @since 1.0.0, 09/04/2014
- */
+@Data
 @Entity
 @Audited
 @ToString(callSuper = true)
@@ -49,50 +39,35 @@ import static br.com.webbudget.infrastructure.utils.DefaultSchemes.FINANCIAL_AUD
 @AuditTable(value = "closings", schema = FINANCIAL_AUDIT)
 public class Closing extends PersistentEntity {
 
-    @Getter
-    @Setter
     @Column(name = "revenues", nullable = false)
     private BigDecimal revenues;
-    @Getter
-    @Setter
+
     @Column(name = "expenses", nullable = false)
     private BigDecimal expenses;
-    @Getter
-    @Setter
+
     @Column(name = "credit_card_expenses", nullable = false)
     private BigDecimal creditCardExpenses;
-    @Getter
-    @Setter
+
     @Column(name = "debit_card_expenses", nullable = false)
     private BigDecimal debitCardExpenses;
-    @Getter
-    @Setter
+
     @Column(name = "cash_expenses", nullable = false)
     private BigDecimal cashExpenses;
-    @Getter
-    @Setter
+
     @Column(name = "balance", nullable = false)
     private BigDecimal balance;
-    @Getter
-    @Setter
+
     @Column(name = "accumulated", nullable = false)
     private BigDecimal accumulated;
-    @Getter
-    @Setter
+
     @Column(name = "closing_date", nullable = false)
     private LocalDate closingDate;
 
-    @Getter
-    @Setter
     @OneToOne
     @JoinColumn(name = "id_financial_period")
     private FinancialPeriod financialPeriod;
 
-    /**
-     * Constructor...
-     */
     public Closing() {
-
         this.balance = BigDecimal.ZERO;
         this.expenses = BigDecimal.ZERO;
         this.revenues = BigDecimal.ZERO;
